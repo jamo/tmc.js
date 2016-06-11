9c35491e02515f3e8bcb7bc301a28dee66a18d67
Jarmo Isotalo <jamo@isotalo.fi>
2016-06-11 19:18:16 +0300

    Script it

---
+++ cron.sh
@@ -0,0 +1,29 @@
+
+export EDITOR=true
+
+if [ -z "$NODE"  ]; then
+  NODE="node"
+fi
+if [ -z "$NPM" ]; then
+  NPM=$(which npm)
+fi
+if [ -z "$SEMVER" ]; then
+  SEMVER="$NODE $($NPM bin)/semver"
+fi
+
+$npm run update >/dev/null
+
+# check if the working tree is "dirty"
+git diff --quiet bin/tmc
+DIRTY=$?
+
+if [ $DIRTY = "1" ]; then
+  # commit the changes to the `index.js` file
+  git add bin/tmc
+  git add package.json
+  VERSION=$($NODE -p "require('./package').version")
+  git commit -m "Update binary to $VERSION"
+  git tag $VERSION
+  #git push origin master
+  $NPM publish
+fi;
